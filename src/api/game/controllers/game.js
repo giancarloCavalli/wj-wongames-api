44edb3096700e8683c6043218cea6aa1acb78fbb
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-04-03 11:48:47 -0300

    feat: add custom route, controller and service in game API

--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const game = require('../routes/game');
+
 /**
  *  game controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::game.game');
+module.exports = createCoreController('api::game.game', ({ strapi }) => ({
+  populate: async (ctx) => {
+    try {
+      console.log("Starting to populate...")
+
+      await strapi.service('api::game.game').populate();
+
+      ctx.send("Finished populating")
+    } catch (err) {
+      ctx.send = err;
+    }
+  }
+}));
--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -6,4 +6,9 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::game.game');
+module.exports = createCoreService('api::game.game', ({ strapi }) => ({
+  populate: async (...args) => {
+    console.log("Calling populate service");
+    await new Promise(r => setTimeout(r, 2000));
+  }
+}));
